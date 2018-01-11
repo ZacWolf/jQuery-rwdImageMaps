c284cc616a36c47fa6934f9b6ade485c62999b67
Zac Morris <zac@zacwolf.com>
2018-01-11 17:48:28 -0500

    Update

--- jquery.rwdImageMaps.min.js
+++ jquery.rwdImageMaps.min.js
@@ -1,4 +1,4 @@
-/*! jquery.rwdImageMaps - v0.1.6 - 2016-07-01
+/*! jquery.rwdImageMaps - v0.1.6 - 2018-01-11
  *
  * Allows image maps to be used in a responsive design by recalculating the area coordinates to match the actual image size on load and window.resize
  *
